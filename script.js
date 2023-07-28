ff1f5b0a2387cbd2205000a0d18f6940b188ad22
unknown <ghimiremilan.gn@gmail.com>
2023-07-29 00:56:52 +0545

    checkboc

--- script.js
+++ script.js
@@ -8,29 +8,34 @@ let arr1=[];
 todoform.addEventListener('submit',(event)=>{
   event.preventDefault();
  
-  const datas={
+  const data={
     id:Date.now(),
     name:todoinput.value,
     completed:false
   }
-  arr1.push(datas);
+  arr1.push(data);
   displaylist();
+  todoform.reset();
  
 
 })
 
  function displaylist(){
-  arr1.forEach((datas)=>{
+  todolist.innerHTML='';
+  arr1.forEach((data)=>{
   const li=document.createElement('li');
   li.classList.add('list-group-item');//creating the list group items in nicer way
   const div=document.createElement("div");
   const checkbox=document.createElement('input');
   checkbox.type='checkbox';
-  checkbox.checked=datas.completed;
+  checkbox.checked=data.completed;
   checkbox.classList.add('form-check-input','me-2');
+  checkbox.addEventListener('change',(event)=>{
+
+  })
 
   const span=document.createElement('span');
-  span.innerText=datas.name;
+  span.innerText=data.name;
   span.classList.add('ms-3');
 
   
