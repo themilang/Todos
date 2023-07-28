d3d7d2404815aa89bbab436fb26adb25a3ef0ec9
unknown <ghimiremilan.gn@gmail.com>
2023-07-29 00:56:33 +0545

    checkboc

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-const todoform=document.querySelector("#todo-form");
-const todoinput=document.querySelector("#form-input");
-const todolist=document.querySelector("#todo-list");
+const todoform=document.querySelector("#todo-form")
+const todoinput=document.querySelector('#form-input')
+const todolist=document.querySelector('#todo-list')
 
 
-const todoarr=[];
+let arr1=[];
 
 todoform.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    const tododata={
-     id:Date.now(),
-     name:todoinput.value,
-     completed:false
-
-    }
-
-  todoarr.push(tododata);
-  console.log(todoarr);
-  dispalylist();
-  todoform.reset();
+  event.preventDefault();
+ 
+  const datas={
+    id:Date.now(),
+    name:todoinput.value,
+    completed:false
+  }
+  arr1.push(datas);
+  displaylist();
+ 
 
 })
-const dispalylist=()=>{
-    todoarr.forEach((tododata)=>{
-        const li=document.createElement('li')
-     li.classlist.add('list-group-item');
-     const div=document.createElement('div');
-     const span=document.createElement('span');
-     span.innerText=tododata.name;
-     span.classlist.add('ms-2');
-     div.append(span)
-     li.append(div);
+
+ function displaylist(){
+  arr1.forEach((datas)=>{
+  const li=document.createElement('li');
+  li.classList.add('list-group-item');//creating the list group items in nicer way
+  const div=document.createElement("div");
+  const checkbox=document.createElement('input');
+  checkbox.type='checkbox';
+  checkbox.checked=datas.completed;
+  checkbox.classList.add('form-check-input','me-2');
+
+  const span=document.createElement('span');
+  span.innerText=datas.name;
+  span.classList.add('ms-3');
+
+  
+  div.append(checkbox);
+  div.append(span);
+  li.append(div);
+  todolist.append(li);
 
 
-    })
+})
+  
 }
